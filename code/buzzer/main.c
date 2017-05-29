247b53d4cd7da7836dbba9efdb28a639dd9fd6c3
daixiang789@gmail.com <daixiang789@gmail.com>
2017-05-29 17:02:27 +0800

    update

--- code/buzzer/main.c
+++ code/buzzer/main.c
@@ -1,6 +1,9 @@
 #define GPD0CON (*(volatile unsigned long*)0xe02000a0)
 #define GPD0DAT (*(volatile unsigned long*)0xe02000a4)
 
+#define GPH0CON (*(volatile unsigned long*)0xe0200c40)
+#define GPH0DAT (*(volatile unsigned long*)0xe0200c44)
+
 void buzzer_on(){
     GPD0DAT |= 0x1;
 }
@@ -16,11 +19,13 @@ void delay(){
 
 int main(void){
     GPD0CON |= 0x1;
+    GPH0CON &= 0x0;
     while(1){
-        buzzer_on();
-        delay();
-        buzzer_off();
-        delay();
+        if(GPH0DAT & 0x1){
+            buzzer_off();
+        }else{
+            buzzer_on();
+        }
     }
     return 0;
 }
